75255ff9f13646b54008de53c25e80221a0e186d
JADundee <Jadundee@icloud.com>
2023-10-13 04:44:45 -0600

    Finished project

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var listItem = document.querySelectorAll("li");
+var listItem = document.getElementsByTagName("li");
+var deleteButton = document.getElementsByClassName("delete") 
 
 
+
+
+
+for (var i = 0; i < deleteButton.length; i++) {
+	deleteButton[i].addEventListener("click", removeParent, false);
+}
+
+function removeParent(e) {
+	e.target.removeEventListener("click", removeParent, false);
+	e.target.parentNode.remove()
+}
+
+/* for (let i = 0; i < listItem.length; i++) {
+	listItem[i].addEventListener("click", function(){
+			listItem[i].classList.toggle("done");
+		})
+	} */
+
+	ul.onclick = function(e){
+		e.target.classList.toggle("done")
+	}
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
+	var btn = document.createElement("button");
+	btn.innerHTML = "X";
+	btn.onclick = removeParent;
+
 	var li = document.createElement("li");
+	li.appendChild(btn)
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
-
 }
 
 function addListAfterClick() {
@@ -29,12 +55,9 @@ function addListAfterKeypress(event) {
 	}
 }
 
-for (let i = 0; i < listItem.length; i++) {
-listItem[i].addEventListener("click", function(){
-		listItem[i].classList.toggle("done")
-	})
-}
 
+console.log(listItem)
+console.log(ul)
 
 
 
@@ -43,9 +66,3 @@ listItem[i].addEventListener("click", function(){
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-
-
-
-console.log(li)
-console.log()
